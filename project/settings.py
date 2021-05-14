9e04dd2f507c371ae66ea4e077ddcd0653e218e2
emansamsalem <emansamirsamir543@gmail.com>
2021-05-14 19:44:16 +0200

    adding main apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'accounts',
+    'property',
+    'blog',
+    'settings',
+    'about'
 ]
 
 MIDDLEWARE = [
